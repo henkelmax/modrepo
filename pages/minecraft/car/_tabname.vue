fb4a5b51983003721e580526d2ee13a6a3cc7284
Max Henkel <max@maxhenkel.de>
2020-10-04 21:10:42 +0200

    Update car FAQ

--- pages/minecraft/car/_tabname.vue
+++ pages/minecraft/car/_tabname.vue
@@ -202,18 +202,14 @@ export default {
         {
           question:
             "I can't find or craft the windshield / axle / control unit",
-          answer: `These items were removed in the 2.X.X versions of this mod. For recipes use <a href="https://www.curseforge.com/minecraft/mc-mods/jei" target="_blank">JEI</a>.`,
+          answer: `These items were removed in newer versions (1.14+) of this mod. For recipes use <a href="https://www.curseforge.com/minecraft/mc-mods/jei" target="_blank">JEI</a>.`,
         },
         {
           question: "How do I fill the tank container?",
           answer: `You can fill the tank container by right-clicking it with a bucket.`,
         },
         {
-          question: "Is version 1.12.2-1.X.X still supported?",
-          answer: `Yes, but it will not get any more feature updates. If you find a bug, please report it.`,
-        },
-        {
-          question: "Is version 1.12.2-2.X.X still supported?",
+          question: "Is version 1.12.2 or lower still supported?",
           answer: `No. I moved on to newer Minecraft versions. This version won't get any bug fix or feature updates.`,
         },
       ],
