1399d02653fa3822a795d425d0ea5687cc034e5e
Max Henkel <max@maxhenkel.de>
2020-11-14 22:18:37 +0100

    Update corpse credits

--- pages/minecraft/corpse/_tabname.vue
+++ pages/minecraft/corpse/_tabname.vue
@@ -13,72 +13,94 @@
   >
     <v-container slot="overview">
       <p>
-        This mod brings a corpse into the game.
-        It will spawn upon your death containing all items that you had in your inventory.
+        This mod brings a corpse into the game. It will spawn upon your death
+        containing all items that you had in your inventory.
       </p>
       <h1>Features</h1>
       <p>
-        The corpse will appear at the players location upon death.
-        Your player skin will be applied to the corpse.
-        It contains all the items you had when you died.
-        It can hold an infinite amount of items.
+        The corpse will appear at the players location upon death. Your player
+        skin will be applied to the corpse. It contains all the items you had
+        when you died. It can hold an infinite amount of items.
       </p>
       <p>You can access the items by right-clicking the corpse.</p>
       <p>
-        When you take out all items, the corpse will disappear.
-        The corpse can’t fall into the void or burn in lava.
-        It also floats in liquids like lava or water.
+        When you take out all items, the corpse will disappear. The corpse can’t
+        fall into the void or burn in lava. It also floats in liquids like lava
+        or water.
       </p>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_1.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_1.png')"
+      ></v-img>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_2.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_2.png')"
+      ></v-img>
       <br />
       <p>
-        After 1 hour, the corpse will turn into a skeleton, indicating that it existed for a longer time.
-        This won’t change the functionality of the corpse.
+        After 1 hour, the corpse will turn into a skeleton, indicating that it
+        existed for a longer time. This won’t change the functionality of the
+        corpse.
       </p>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_3.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_3.png')"
+      ></v-img>
       <br />
       <p>
-        You get all your items back into their original slots when transferring the items.
+        You get all your items back into their original slots when transferring
+        the items.
       </p>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_4.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_4.png')"
+      ></v-img>
       <br />
       <h2>Death History</h2>
       <p>
-        You can view all your past deaths by pressing the ‘U’ key.
-        You can retrieve lost items if you are in creative mode.
+        You can view all your past deaths by pressing the ‘U’ key. You can
+        retrieve lost items if you are in creative mode.
       </p>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_5.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_5.png')"
+      ></v-img>
       <br />
       <h2>Viewing the Death Hisory of other Players</h2>
       <p>
         By typing
-        <code>/deathhistory &lt;player&gt;</code> as an operator, you can view the death history of another player.
+        <code>/deathhistory &lt;player&gt;</code> as an operator, you can view
+        the death history of another player.
       </p>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_6.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_6.png')"
+      ></v-img>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_7.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_7.png')"
+      ></v-img>
       <br />
       <h2>Recovering Lost Items</h2>
       <p>
-        You can retrieve lost items by pressing the “Items” button in the death history GUI.
-        Note that you can only take out items if you are in creative mode.
+        You can retrieve lost items by pressing the “Items” button in the death
+        history GUI. Note that you can only take out items if you are in
+        creative mode.
       </p>
       <h2>Teleporting to the Death Location</h2>
       <p>
-        By clicking on the “Location” area in the death history GUI, the command to teleport you to the death location will be displayed in chat.
-        It includes the coordinates and the dimension.
+        By clicking on the “Location” area in the death history GUI, the command
+        to teleport you to the death location will be displayed in chat. It
+        includes the coordinates and the dimension.
       </p>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_8.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_8.png')"
+      ></v-img>
       <br />
-      <v-img :src="require('~/assets/minecraft/corpse/image_corpse_9.png')"></v-img>
+      <v-img
+        :src="require('~/assets/minecraft/corpse/image_corpse_9.png')"
+      ></v-img>
       <br />
       <h2>Optional Features (Config Options)</h2>
       <ul>
@@ -144,6 +166,13 @@ export default {
           name: {
             name: "edmory",
             link: "https://www.curseforge.com/members/edmory",
+          },
+        },
+        {
+          element: { name: "Russian translation" },
+          name: {
+            name: "DrHesperusModoVestnic",
+            link: "https://github.com/DrHesperusModoVestnic",
           },
         },
       ],
