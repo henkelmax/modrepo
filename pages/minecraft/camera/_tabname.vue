24d8d0dc1a18a3bc795d823380b7a0d58c17f4d8
Max Henkel <max@maxhenkel.de>
2021-10-06 13:10:49 +0200

    Update camera credits

--- pages/minecraft/camera/_tabname.vue
+++ pages/minecraft/camera/_tabname.vue
@@ -201,6 +201,13 @@ export default {
             link: "https://www.curseforge.com/members/prosplash1",
           },
         },
+        {
+          element: { name: "French translation" },
+          name: {
+            name: "BlackJamesYT",
+            link: "https://github.com/BlackJamesYT",
+          },
+        },
       ],
     };
   },
