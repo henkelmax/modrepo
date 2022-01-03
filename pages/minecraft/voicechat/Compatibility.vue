085b7a6735b01f5c18d4ad26e9b2eadc66162cfe
Max Henkel <max@maxhenkel.de>
2022-01-03 14:37:57 +0100

    Update compatibility

--- pages/minecraft/voicechat/Compatibility.vue
+++ pages/minecraft/voicechat/Compatibility.vue
@@ -1,22 +1,35 @@
 <template>
   <div>
-    <h1>Compatibility between versions</h1>
+    <h1>Compatibility between mod versions</h1>
 
     <p>
-      Generally it is always recommended, that you use the exact same version on server and client.
+      Generally it is always recommended, that you use the exact same version on
+      server and client.
     </p>
 
     <p>
       This mod follows <a href="https://semver.org/">Semantic Versioning</a>.
-      This means version <code>1.2.3</code> has a major version of <code>1</code>, a minor version of <code>2</code> and a patch version of <code>3</code>.
-      So, as a rule of thumb, only the major and minor versions need to match.
-      So for example you can join a server with version <code>1.2.3</code>, your client can also have version <code>1.2.0</code> or <code>1.2.5</code>.
+      This means version <code>1.2.3</code> has a major version of
+      <code>1</code>, a minor version of <code>2</code> and a patch version of
+      <code>3</code>. So, as a rule of thumb, only the major and minor versions
+      need to match. So for example you can join a server with version
+      <code>1.2.3</code>, your client can also have version
+      <code>1.2.0</code> or <code>1.2.5</code>.
     </p>
+
+    <h1>Compatibility between Minecraft versions</h1>
 
     <p>
-      This doesn't mean the mod is compatible across Minecraft versions.
-      Mods or plugins like ViaVersion are also not supported.
+      Since version <code>2.1.0</code> of Simple Voice Chat, the mod is
+      compatible between Minecraft versions with the help of tools like
+      <a href="https://www.spigotmc.org/resources/viaversion.19254/"
+        >ViaVersion</a
+      >
+      or
+      <a href="https://www.spigotmc.org/resources/viabackwards.27448/"
+        >ViaBackwards</a
+      >. But you generally need to respect the compatibility between mod
+      versions as described above.
     </p>
-
   </div>
 </template>
