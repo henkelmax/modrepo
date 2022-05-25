786489909349af0705d42191f71688d064d7820f
Max Henkel <max@maxhenkel.de>
2022-05-25 16:37:57 +0200

    Update voicechat wiki

--- pages/minecraft/voicechat/Icons.vue
+++ pages/minecraft/voicechat/Icons.vue
@@ -2,8 +2,6 @@
   <div>
     <h1>Icons</h1>
 
-    <p>Maybe you have noticed that the icons of the voice chat have changed.</p>
-
     <p>
       If you want the old icons back, you can select the classic icons in the
       resource pack menu. Alternatively, there are also options for all icons to
