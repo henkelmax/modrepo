4f922984f00ca015989069d320fa6d4ddef6decb
Max Henkel <max@maxhenkel.de>
2022-05-26 16:37:57 +0200

    Update voicechat wiki icon section

--- pages/minecraft/voicechat/Icons.vue
+++ pages/minecraft/voicechat/Icons.vue
@@ -3,10 +3,26 @@
     <h1>Icons</h1>
 
     <p>
-      If you want the old icons back, you can select the classic icons in the
-      resource pack menu. Alternatively, there are also options for all icons to
-      have white or black outlines.
+      The mod offers a choice of different icons. These are automatically
+      available for selection in the resourcepack settings.
     </p>
+
+    <p>The following icons are available:</p>
+
+    <ul>
+      <li>
+        <b>Black Icons</b> - All icons have black outlines instead of white ones
+      </li>
+      <li>
+        <b>White Icons</b> - All icons have white outlines, including buttons
+      </li>
+      <li>
+        <b>Classic Icons</b> - Restores the old classic icons from older
+        versions
+      </li>
+    </ul>
+
+    <br />
 
     <v-img
       :src="require('~/assets/minecraft/voicechat/image_voicechat_12.png')"
