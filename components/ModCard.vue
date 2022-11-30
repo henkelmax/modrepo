402c389442586d9e2dcb247173d1ef81d566e855
Max Henkel <max@maxhenkel.de>
2022-11-30 15:26:38 +0100

    Fix mod cards not opening in new tab Closes #119

--- components/ModCard.vue
+++ components/ModCard.vue
@@ -1,17 +1,25 @@
 <template>
   <v-card outlined height="100%">
-    <v-img :src="logo" class="pointer" @click="push(overviewPath)"></v-img>
+    <nuxt-link :to="overviewPath" style="text-decoration: none !important">
+      <v-img :src="logo" class="pointer"></v-img>
+    </nuxt-link>
     <v-card-title>{{ name }}</v-card-title>
     <v-card-subtitle>{{ description }}</v-card-subtitle>
     <v-card-actions>
-      <v-btn color="primary" text @click="push(overviewPath)">More</v-btn>
+      <nuxt-link :to="overviewPath" style="text-decoration: none !important">
+        <v-btn color="primary" text>More</v-btn>
+      </nuxt-link>
       <v-spacer></v-spacer>
-      <v-btn v-if="wikiPath" icon @click="push(wikiPath)">
-        <v-icon>mdi-information-variant</v-icon>
-      </v-btn>
-      <v-btn v-if="imagesPath" icon @click="push(imagesPath)">
-        <v-icon>mdi-image-size-select-actual</v-icon>
-      </v-btn>
+      <nuxt-link :to="wikiPath" style="text-decoration: none !important">
+        <v-btn v-if="wikiPath" icon>
+          <v-icon>mdi-information-variant</v-icon>
+        </v-btn>
+      </nuxt-link>
+      <nuxt-link :to="imagesPath" style="text-decoration: none !important">
+        <v-btn v-if="imagesPath" icon>
+          <v-icon>mdi-image-size-select-actual</v-icon>
+        </v-btn>
+      </nuxt-link>
       <v-btn v-if="downloadLink" icon :href="downloadLink" target="_blank">
         <v-icon>mdi-download</v-icon>
       </v-btn>
