3238cdba4794f3b26a047f125381102b4233717d
Max Henkel <max@maxhenkel.de>
2022-12-16 10:15:37 +0100

    Add issues section to voice chat

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,6 +27,7 @@ export default {
             { text: 'Credits', link: '/minecraft/voicechat/credits.md' },
             { text: 'Downloads', link: '/minecraft/voicechat/downloads.md' },
             { text: 'Discord', link: 'https://discord.gg/4dH2zwTmyX' },
+            { text: 'Issues', link: 'https://github.com/henkelmax/simple-voice-chat/issues' },
             { text: 'GitHub', link: 'https://github.com/henkelmax/simple-voice-chat' },
             { text: 'API', link: 'https://github.com/henkelmax/simple-voice-chat/tree/1.19.3/api' },
           ]
