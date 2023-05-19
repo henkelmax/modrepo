5c4be8e9a203ddefc83f6ee9bff6c36f761c65bd
Max Henkel <max@maxhenkel.de>
2023-05-19 09:13:59 +0200

    Add download redirects

--- docs/components/Redirect.vue
+++ docs/components/Redirect.vue
@@ -1,12 +1,20 @@
 <template>
-    <meta http-equiv="Refresh" :content="`0; url='${path}'`" />
+  <meta http-equiv="refresh" :content="`0; url='${path}'`" />
+  <h1>Redirecting...</h1>
+  <p>
+    Please click <a :href="path">here</a> if you are not getting redirected.
+  </p>
 </template>
 
 <script setup>
-defineProps({
+const props = defineProps({
   path: {
     type: String,
     required: true,
   },
 });
+
+setTimeout(() => {
+  window.open(props.path, "_self");
+}, 1000);
 </script>
