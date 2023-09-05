15d641c1d32ecdea33f468d9c1616d70e39a582d
Max Henkel <max@maxhenkel.de>
2023-09-05 19:42:28 +0200

    Update vitepress and fix theme

--- docs/components/Theme.vue
+++ docs/components/Theme.vue
@@ -1,9 +1,24 @@
-<template>
-  
-</template>
+<template></template>
 
 <style>
 :root {
   --vp-c-brand: #42b883 !important;
+  --vp-c-brand-1: #42b883 !important;
+}
+
+a:link {
+  text-decoration: none;
+}
+
+a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+a:active {
+  text-decoration: none;
 }
 </style>
