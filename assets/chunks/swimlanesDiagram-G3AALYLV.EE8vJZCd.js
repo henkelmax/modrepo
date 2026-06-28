b0b618dbdc7cd5936f8ca068e3aa4b203f86614a
henkelmax <max@maxhenkel.de>
2026-06-28 07:56:23 +0000

    Deploying to gh-pages from @ henkelmax/modrepo@f181cec0683966bc7c082b6dc3ff250c7e3e29c8 🚀

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.EE8vJZCd.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.BLPLM2O2.js";import{_ as a}from"../app.Dk9yB_o5.js";import"./chunk-5VM5RSS4.5VufWOTP.js";import"./chunk-XXDRQBXY.BKUoU0Yr.js";import"./chunk-VR4S4FIN.DWrVnb3q.js";import"./chunk-32BRIVSS.Dj11L4_m.js";import"./channel.CFYdsrBW.js";import"./framework.BL_GDCtl.js";import"./theme.BIAoUTpB.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
